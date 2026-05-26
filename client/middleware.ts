b87ad5a15e068e17a3a8067c5f98c63b6db0b9b9
navid001 <navidalvi.001@gmail.com>
2026-05-26 06:48:59 +0600

    Added proper middleware for  session management

---
+++ client/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("access_token");
+
+  if (!token) {
+    const loginUrl = new URL("/", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/assign/:path*", "/ship/:path*", "/pos/:path*"],
+};
